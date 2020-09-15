cb3b412fedf3ae0d8b81e1df68650cefa4312ae6
LeiLiuwudi <2364485120@qq.com>
2020-09-15 16:30:14 +0800

    '修改启动类允许跨域'

--- src/main/java/com/zju/gcs/RehabServerApplication.java
+++ src/main/java/com/zju/gcs/RehabServerApplication.java
@@ -3,13 +3,23 @@ package com.zju.gcs;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @MapperScan("com.zju.gcs.mapper")
-public class RehabServerApplication {
+public class RehabServerApplication extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(RehabServerApplication.class, args);
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .allowedOrigins("*")
+                .allowedMethods("*");
+    }
 }
